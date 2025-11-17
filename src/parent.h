a989eb4786baedf8f53efe023ebdf084808cc9ca
Ahmad Rofi' Izzulhaq <ahrofzulcaruban@gmail.com>
2025-11-17 22:42:08 +0700

    update parent.h

--- src/parent.h
+++ src/parent.h
@@ -1,1 +1,31 @@
+#ifndef PARENT_H
+#define PARENT_H
+
+#include <iostream>
+#include <string>
+using std::string;
+using std::cout;
+using std::endl;
+
+struct Parent {
+    string id;            // unique key
+    string hostname;
+    string ip;
+    int uptimeHours;      // contoh atribut integer
+    Parent* next;
+};
+
+struct ParentList {
+    Parent* first;
+};
+
+// ADT Parent
+void createParentList(ParentList &L);
+Parent* allocateParent(const string &id, const string &hostname, const string &ip, int uptimeHours);
+void insertParent(ParentList &L, Parent* P);
+Parent* findParent(ParentList L, const string &id);
+bool deleteParent(ParentList &L, const string &id); // returns true if deleted
+void showAllParents(ParentList L);
+
+#endif
 
