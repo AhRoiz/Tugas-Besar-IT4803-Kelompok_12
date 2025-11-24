a83688c5653cd80e960a25792546c872bfb08ff9
Ja'far Anafa Pulungan <jafaranafa1@gmail.com>
2025-11-24 15:02:23 +0700

    update ADT

--- src/Mobil.h
+++ src/Mobil.h
@@ -10,36 +10,36 @@ using namespace std;
 struct Mobil {
     string idMobil;
     string Merek;
-    string Model
+    string Model;
     int harga;
     int tahunProduksi;
     string status;
 };
 
-typedef struct Mobil infotype:
+typedef struct Mobil infotype;
 
-typedef struct elmMobil *address;
+typedef struct elmMobil *adrMobil;
 
 struct elmMobil {
     infotype info;
-    address next;
+    adrMobil next;
 };
 
 struct ListMobil {
-    address first;
+    adrMobil first;
 };
 
 // Primitif
 void createListMobil(ListMobil &L);
-address allocate(infotype x);
+adrMobil allocate(infotype x);
 void createListMobil(ListMobil &L);
-address allocate(infotype x);
-void insertFirst(ListMobil &L, address p);
-void inserLast(ListMobil &L, address p);
-void insertafter(ListMobil &L,address prec, address &p);
-void deleteFirst(ListMobil &L, address p);
-void deleteLast(ListMobil &L, address p);
-void deleteAfter(ListMobil &L, address prec, address &p);
+adrMobil allocate(infotype x);
+void insertFirst(ListMobil &L, adrMobil p);
+void inserLast(ListMobil &L, adrMobil p);
+void insertafter(ListMobil &L,adrMobil prec, adrMobil &p);
+void deleteFirst(ListMobil &L, adrMobil p);
+void deleteLast(ListMobil &L, adrMobil p);
+void deleteAfter(ListMobil &L, adrMobil prec, adrMobil &p);
 void printInfo(ListMobil L);
 void findMobil(ListMobil L);
 void showAllMobil(ListMobil L);
