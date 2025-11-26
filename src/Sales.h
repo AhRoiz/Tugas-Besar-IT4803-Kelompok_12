0bd42d952ed00b1d8fe75a8a817dd54db011b841
Ja'far Anafa Pulungan <jafaranafa1@gmail.com>
2025-11-26 18:44:03 +0700

    revisi ADT

--- src/Sales.h
+++ src/Sales.h
@@ -1,5 +1,5 @@
-#ifndef SALES_H_INCLUDED
-#define SALES_H_INCLUDED
+#ifndef SALsES_H_INCLsUDED
+#define SALsES_H_INCLsUDED
 
 #include <iostream>
 #include <string>
@@ -27,14 +27,14 @@ struct ListSales {
 };
 
 // Primitif
-void createListSales(ListSales &L);
+void createListSales(ListSales &Ls);
 adrSales allocateSales(infotype x);
-void insertFirst(ListSales &L, adrSales p);
-void inserLast(ListSales &L, adrSales p);
-void insertafter(ListSales &L,adrSales prec, adrSales p);
-void deleteFirst(ListSales &L, adrSales &p);
-void deleteLast(ListSales &L, adrSales &p);
-void deleteAfter(ListSales &L, adrSales prec, adrSales &p);
-void findSales(ListSales L);
-void showAllSales(ListSales L);
+void insertFirst(ListSales &Ls, adrSales p);
+void inserLast(ListSales &Ls, adrSales p);
+void insertafter(ListSales &Ls,adrSales prec, adrSales p);
+void deleteFirst(ListSales &Ls, adrSales &p);
+void deleteLast(ListSales &Ls, adrSales &p);
+void deleteAfter(ListSales &Ls, adrSales prec, adrSales &p);
+void findSales(ListSales Ls);
+void showAllSales(ListSales Ls);
 #endif
