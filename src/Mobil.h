9bb70c47a71d506df0c8740c091d895eb550cb89
Ja'far Anafa Pulungan <jafaranafa1@gmail.com>
2025-11-26 19:11:17 +0700

    revisi ADT

--- src/Mobil.h
+++ src/Mobil.h
@@ -30,18 +30,16 @@ struct ListMobil {
 };
 
 // Primitif
-void createListMobil(ListMobil &L);
-adrMobil allocate(infotype x);
-void createListMobil(ListMobil &L);
-adrMobil allocate(infotype x);
-void insertFirst(ListMobil &L, adrMobil p);
-void inserLast(ListMobil &L, adrMobil p);
-void insertafter(ListMobil &L,adrMobil prec, adrMobil &p);
-void deleteFirst(ListMobil &L, adrMobil p);
-void deleteLast(ListMobil &L, adrMobil p);
-void deleteAfter(ListMobil &L, adrMobil prec, adrMobil &p);
-void printInfo(ListMobil L);
-void findMobil(ListMobil L);
-void showAllMobil(ListMobil L);
+
+void createListMobil(ListMobil &Lm);
+adrMobil allocateMobil(infotype x);
+void insertFirstMobil(ListMobil &Lm, adrMobil p);
+void inserLastMobil(ListMobil &Lm, adrMobil p);
+void insertafterMobil(ListMobil &Lm,adrMobil prec, adrMobil p);
+void deleteFirstMobil(ListMobil &Lm, adrMobil &p);
+void deleteLastMobil(ListMobil &Lm, adrMobil &p);
+void deleteAfterMobil(ListMobil &Lm, adrMobil prec, adrMobil &p);
+void findMobil(ListMobil Lm);
+void showAllMobil(ListMobil Lm);
 #endif
 
