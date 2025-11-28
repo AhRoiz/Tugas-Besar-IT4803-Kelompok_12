5b9c5ffdfac3f4960aadae22da46e14913361614
Ahmad Rofi' Izzulhaq <ahrofzulcaruban@gmail.com>
2025-11-28 13:20:51 +0700

    feeling cute, might delete later

--- src/Sales_103032400045.cpp
+++ src/Sales_103032400045.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include "Sales.h"
+using namespace std;
 
+void insertAfterSales(ListSales &L, adrSales prec, adrSales p) {
+    if (prec != NULL) {
+        if (prec == L.last) {
+            insertLastSales(L, p);
+        } else {
+            p->next = prec->next;
+            p->prev = prec;
+            prec->next->prev = p;
+            prec->next = p;
+        }
+    }
+}
