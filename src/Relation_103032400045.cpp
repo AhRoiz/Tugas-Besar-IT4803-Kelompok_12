37995b58faa405fa46be5453697320d2a4b2351c
Ahmad Rofi' Izzulhaq <ahrofzulcaruban@gmail.com>
2025-12-02 16:06:51 +0700

    update relasi

--- src/Relation_103032400045.cpp
+++ src/Relation_103032400045.cpp
@@ -1,1 +1,45 @@
+#include "Relation.h"
+#include <iostream>
 
+using namespace std;
+
+void createListRelasi(ListRelasi &L) {
+    L.first = nullptr;
+    L.last = nullptr;
+}
+
+adrRelasi alokasiRelasi(adrSales P, adrMobil C) {
+    adrRelasi R = new elmRelasi;
+    R->parent = P;
+    R->child = C;
+    R->next = nullptr;
+    R->prev = nullptr;
+
+    return R;
+}
+
+void insertFirstRelasi(ListRelasi &L, adrRelasi R) {
+    if (L.first == nullptr) {
+        L.first = R;
+        L.last = R; 
+    } else {
+        R->next = L.first;
+        L.first->prev = R;
+        L.first = R;
+    }
+}
+
+void insertLastRelasi(ListRelasi &L, adrRelasi R) {
+    if (L.first == nullptr) {
+        L.first = R;
+        L.last = R; 
+    } else {
+        L.last->next = R;
+        R->prev = L.last;
+        L.last = R;
+    }
+}
+
+void insertAfterRelasi(ListRelasi &L, adrRelasi Prec, adrRelasi R) {
+    
+}
